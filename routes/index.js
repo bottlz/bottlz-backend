6351bfb6b7a6a2493561705103eb5367a0d14691
Benjamin Shen <bfs45@cornell.edu>
2022-10-19 21:19:11 -0400

    Setup prettier and eslint (#2)
    * add prettier and eslint
    
    * format

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bottlz' });
+// eslint-disable-next-line no-unused-vars
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Bottlz" });
 });
 
 module.exports = router;
