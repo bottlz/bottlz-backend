3e700caf99f95f0cff9ac869eca67735b73bbf34
Benjamin Shen <bfs45@cornell.edu>
2022-10-26 21:46:03 -0400

    update configs

--- dbConfig.js
+++ dbConfig.js
@@ -1,21 +1,20 @@
 /* eslint-disable no-console */
+require("dotenv").config();
+
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 const BottlesDao = require("./models/bottlesDao");
 
-const dotenv = require("dotenv");
-dotenv.config();
-
 const config = {};
 
-config.host = "https://bottles-db.documents.azure.com:443/";
-config.authKey = process.env.COSMOS_DB_KEY;
+config.dbHost = "https://bottles-db.documents.azure.com:443/";
+config.dbKey = process.env.COSMOS_DB_KEY;
 config.databaseId = "bottles";
 config.containerId = "bottles-container1";
 
 // set up database
 const cosmosClient = new CosmosClient({
-  endpoint: config.host,
-  key: config.authKey,
+  endpoint: config.dbHost,
+  key: config.dbKey,
 });
 const bottlesDao = new BottlesDao(
   cosmosClient,
