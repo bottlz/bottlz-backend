224eba4db0f1ac7dbb7357a854a733c1c0ca13f0
Benjamin Shen <bfs45@cornell.edu>
2022-10-28 22:09:13 -0400

    Use GeoJSON and update bottles routes (#8)
    use geojson, update routes

--- routes/bottles.js
+++ routes/bottles.js
@@ -3,28 +3,30 @@ const router = express.Router();
 
 const { bottlesDao } = require("../dbConfig");
 
+const Location = require("../models/location");
+
+const NEARBY_KM = 3000;
+
 async function getBottle(id) {
   const res = await bottlesDao.getItem(id);
-  return { res };
-}
-
-async function getNearbyBottles(location) {
-  // TODO implement this function using geojson
-  return { TODO: location };
-}
-
-async function getAllBottles() {
-  // TODO fix query
-  const res = await bottlesDao.find({
-    query: "SELECT * FROM bottles.id",
-  });
   return res;
 }
 
+async function getNearbyBottles(location) {
+  const userLocation = new Location(location).get();
+  const bottles = await bottlesDao.find(`SELECT * FROM bottles b
+  WHERE ST_DISTANCE(b.origin, ${JSON.stringify(userLocation)}) < ${NEARBY_KM}`);
+  return { location: userLocation, bottles };
+}
+
+async function getAllBottles() {
+  const bottles = await bottlesDao.getAllItems();
+  return { bottles };
+}
+
 async function createBottle(location) {
-  // TODO replace with geojson
   const { id } = await bottlesDao.addItem({
-    origin: new Location(location).dao(),
+    origin: new Location(location).get(),
   });
   // TODO call routes function
   return { id };
@@ -36,8 +38,8 @@ router.get("/", async function (req, res) {
 });
 
 router.get("/:id", async function (req, res) {
-  const bottle = await getBottle(req.params.id);
-  res.send(bottle);
+  const response = await getBottle(req.params.id);
+  res.send(response);
 });
 
 router.get("/:id/view", async function (req, res) {
@@ -46,7 +48,7 @@ router.get("/:id/view", async function (req, res) {
   res.send(response);
 });
 
-router.post("/", async function (req, res) {
+router.post("/create", async function (req, res) {
   const response = await createBottle(req.body.location);
   res.send(response);
 });
