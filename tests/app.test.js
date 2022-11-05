c9cc3f6514e5eac3d880a6538aa049c39c76fcc2
Benjamin Shen <bfs45@cornell.edu>
2022-11-05 16:43:14 -0400

    Improve error handling, add tests for bad paths (#11)
    improve error handling

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,9 +2,23 @@ const app = require("../app");
 const supertest = require("supertest");
 const request = supertest(app);
 
-describe("/ endpoint", () => {
-  it("should return a response", async () => {
+describe("sanity check", () => {
+  it("/ should return 200", async () => {
     const response = await request.get("/");
     expect(response.status).toBe(200);
+  });
+});
+
+describe("/bottles requests", () => {
+  test.each`
+    path         | method         | expected
+    ${"/"}       | ${request.get} | ${404}
+    ${"/get"}    | ${request.get} | ${404}
+    ${"/view"}   | ${request.get} | ${404}
+    ${"/create"} | ${request.get} | ${404}
+    ${"/nearby"} | ${request.get} | ${404}
+  `("bad paths", async ({ path, method, expected }) => {
+    const response = await method(`/bottles${path}`);
+    expect(response.status).toBe(expected);
   });
 });
